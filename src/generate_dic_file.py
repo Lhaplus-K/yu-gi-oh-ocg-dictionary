51bc5755d9c1a700fd886d7cf428d0fe112fca4a
Lhaplus-K <lhaplus.k@gmail.com>
2024-01-28 17:35:26 +0900

    add generate process and file

---
+++ src/generate_dic_file.py
@@ -0,0 +1,19 @@
+import pandas
+import zipfile
+from os import path
+
+READ_CSV_PATH = 'in/yugioh-dic.csv'
+OUT_DIR = 'out/'
+OUT_FILE_NAME = 'yugioh-dic'
+EXT_TEXT = '.txt'
+EXT_ZIP = '.zip'
+
+def generate():
+  file_base_path = path.join(OUT_DIR, OUT_FILE_NAME)
+  df = pandas.read_csv(READ_CSV_PATH)
+  df.to_csv(path_or_buf=file_base_path + EXT_TEXT, sep='\t', header=False, index=False, encoding='utf-8')
+  with zipfile.ZipFile(file=file_base_path + EXT_ZIP, mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
+    zf.write(filename=file_base_path + EXT_TEXT, arcname=OUT_FILE_NAME + EXT_TEXT)
+
+if __name__ == '__main__':
+  generate()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from generate_dic_file import generate
+
+if __name__ == '__main__':
+  generate()
