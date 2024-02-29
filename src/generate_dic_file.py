2fc245f834263333d8e40acd73dde0b170d9d0fd
Lhaplus-K <lhaplus.k@gmail.com>
2024-02-29 22:54:52 +0900

    リファクタリング

--- src/generate_dic_file.py
+++ src/generate_dic_file.py
@@ -10,7 +10,7 @@ OUT_FILE_NAME = 'yugioh-dic'
 EXT_PLIST = '.plist'
 EXT_TEXT = '.txt'
 EXT_ZIP = '.zip'
-XML = '<?xml version="1.0" encoding="utf-8"?>'
+XML_HEADER = '<?xml version="1.0" encoding="utf-8"?>'
 DOCTYPE = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
 
 def generate():
@@ -40,10 +40,9 @@ def generate_for_plist(data_frame: DataFrame, file_base_path: str):
   array_node = ET.SubElement(root, 'array')
   for row in df.itertuples():
     add_plist_node(array_node, row.Word, row.Reading)
-  pretty_print(root)
-  tree = ET.ElementTree(root)
+  tree = ET.ElementTree(pretty_print(root))
   with open(f'{file_base_path}_mac{EXT_PLIST}', 'wb') as file:
-    file.write(f'{XML}\n'.encode('utf8'))
+    file.write(f'{XML_HEADER}\n'.encode('utf8'))
     file.write(f'{DOCTYPE}\n'.encode('utf8'))
     tree.write(file, encoding='utf-8', xml_declaration=False)
 
@@ -53,19 +52,18 @@ def add_plist_node(parent: ET.Element, word: str, reading: str):
   add_xml_child_node(dict_node, 'string', word)
   add_xml_child_node(dict_node, 'key', 'shortcut')
   add_xml_child_node(dict_node, 'string', reading)
-  return dict_node
 
-def add_xml_child_node(parent: ET.Element, tag: str, text: str = None):
+def add_xml_child_node(parent: ET.Element, tag: str, text: str=None):
   child = ET.SubElement(parent, tag)
   if text is not None:
     child.text = text
   return child
 
-def pretty_print(current: ET.Element, parent: ET.Element = None, index: int = -1, depth: int = 0):
+def pretty_print(current: ET.Element, parent: ET.Element=None, index: int=-1, depth: int=0):
   for i, node in enumerate(current):
     pretty_print(node, current, i, depth + 1)
   if parent is None:
-    return
+    return current
   if index == 0:
     indent = "\t" * depth
     parent.text = f'\n{indent}'
