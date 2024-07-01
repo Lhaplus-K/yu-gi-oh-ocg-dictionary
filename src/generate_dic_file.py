05a9711cbfcc17da6a68aacaad62b9e760e18962
Lhaplus-K <lhaplus.k@gmail.com>
2024-07-01 22:07:45 +0900

    変換先ファイルのSuffixを定数で指定

--- src/generate_dic_file.py
+++ src/generate_dic_file.py
@@ -11,6 +11,9 @@ OUT_FILE_NAME = 'yugioh-dic'
 EXT_PLIST = '.plist'
 EXT_TEXT = '.txt'
 EXT_ZIP = '.zip'
+SUFFIX_MAC = 'mac'
+SUFFIX_PLIST = 'mac'
+SUFFIX_UTF16LE = 'utf16le'
 XML_HEADER = '<?xml version="1.0" encoding="utf-8"?>'
 DOCTYPE = '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
 
@@ -44,11 +47,13 @@ def generate_for_mac(data_frame: DataFrame, file_base_path: str):
   df = data_frame
   df = df.replace('固有名詞', 'その他の固有名詞')
   df = df.replace('短縮よみ', 'その他の固有名詞')
-  df.to_csv(path_or_buf=f'{file_base_path}_mac{EXT_TEXT}', sep=',', header=False, index=False, encoding='utf-8')
+  suffix = SUFFIX_MAC
+  df.to_csv(path_or_buf=f'{file_base_path}_{suffix}{EXT_TEXT}', sep=',', header=False, index=False, encoding='utf-8')
 
 def generate_for_utf16le(data_frame: DataFrame, file_base_path: str):
   df = data_frame
-  df.to_csv(path_or_buf=f'{file_base_path}_utf16le{EXT_TEXT}', sep='\t', header=False, index=False, encoding='utf-16')
+  suffix = SUFFIX_UTF16LE
+  df.to_csv(path_or_buf=f'{file_base_path}_{suffix}{EXT_TEXT}', sep='\t', header=False, index=False, encoding='utf-16')
 
 def generate_for_plist(data_frame: DataFrame, file_base_path: str):
   df = data_frame
@@ -58,7 +63,8 @@ def generate_for_plist(data_frame: DataFrame, file_base_path: str):
   for row in df.itertuples():
     add_plist_node(array_node, row.Word, row.Reading)
   tree = ET.ElementTree(pretty_print(root))
-  with open(f'{file_base_path}_mac{EXT_PLIST}', 'wb') as file:
+  suffix = SUFFIX_PLIST
+  with open(f'{file_base_path}_{suffix}{EXT_PLIST}', 'wb') as file:
     file.write(f'{XML_HEADER}\n'.encode('utf8'))
     file.write(f'{DOCTYPE}\n'.encode('utf8'))
     tree.write(file, encoding='utf-8', xml_declaration=False)
